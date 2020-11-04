78ad9a5d4b0f71217b1d768b572f18e6de60489b
msikorski <235076@student.pwr.edu.pl>
2020-11-04 14:49:11 +0100

    dodano user i logowanie

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\UserAccounts;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+     {
+         $this->passwordEncoder = $passwordEncoder;
+     }
+    public function load(ObjectManager $manager)
+    {
+        $user = new UserAccounts();
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'admin'
+                     ));
+        $user->setEmail("admin@admin.pl");
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
